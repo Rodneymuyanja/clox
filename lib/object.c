6f94883a2283e42ae430957e4c8fe1fdbeb200d9
muyanja <muyanjarodney266@gmail.com>
2025-01-12 01:45:16 +0300

    Started on the hash table

--- lib/object.c
+++ lib/object.c
@@ -14,16 +14,18 @@ static Obj* allocate_object(size_t size, ObjType type);
 
 
 ObjString* copy_string(const char* chars, int length){
+    uint32_t hash = hash_string(chars, length);
     char* heap_chars = ALLOCATE(char, length + 1);
     memcpy(heap_chars, chars, length);
     heap_chars[length] = '\0';
-    return allocate_string(heap_chars, length);
+    return allocate_string(heap_chars, length, hash);
 }
 
-static ObjString* allocate_string(char* chars, int length){
+static ObjString* allocate_string(char* chars, int length, uint32_t hash){
     ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
     string->length = length;
     string->chars = chars;
+    string->hash = hash;
     return string;
 }
 
@@ -46,5 +48,16 @@ void print_object(Value value){
 }
 
 ObjString* take_string(char* chars, int length){
-    return allocate_string(chars, length);
+    uint32_t hash = hash_string(chars, length);
+    return allocate_string(chars, length,hash);
+}
+
+/*FNV-1a*/
+static uint32_t hash_string(const char* key, int length){
+    uint32_t hash = 2166136261u;
+    for (int i = 0; i < length; i++){
+        hash ^= (uint8_t)key[i];
+        hash *= 16777619;
+    }
+    return hash;
 }
