1f820776d04ae6b4eab4ee140e40123a7288eb7a
muyanja <muyanjarodney266@gmail.com>
2025-01-13 22:05:05 +0300

    Finished the hashtable implementation and string interning

--- lib/object.c
+++ lib/object.c
@@ -4,17 +4,23 @@
 #include "memory.h"
 #include "object.h"
 #include "value.h"
+#include "table.h"
 #include "vm.h"
 
 #define ALLOCATE_OBJ(type, object_type) \
         (type*)allocate_object(sizeof(type), object_type)
 
-static ObjString* allocate_string(char* chars, int length);
+static ObjString* allocate_string(char* chars, int length, uint32_t hash);
 static Obj* allocate_object(size_t size, ObjType type);
 
 
 ObjString* copy_string(const char* chars, int length){
     uint32_t hash = hash_string(chars, length);
+    ObjString* interned_string = table_find_string(&vm.strings,chars,length,hash);
+
+
+    if(interned_string != NULL) return interned_string;
+
     char* heap_chars = ALLOCATE(char, length + 1);
     memcpy(heap_chars, chars, length);
     heap_chars[length] = '\0';
@@ -26,6 +32,7 @@ static ObjString* allocate_string(char* chars, int length, uint32_t hash){
     string->length = length;
     string->chars = chars;
     string->hash = hash;
+    table_set(&vm.strings,string,NIL_VAL);
     return string;
 }
 
@@ -47,8 +54,19 @@ void print_object(Value value){
     }
 }
 
+/* 
+    this function takes ownership of the copied string
+    meaning it's responsible for freeing that memory
+ */
 ObjString* take_string(char* chars, int length){
     uint32_t hash = hash_string(chars, length);
+    ObjString* interned_string = table_find_string(&vm.strings,chars, length,hash);
+
+    if(interned_string != NULL){
+        FREE_ARRAY(char, chars, length + 1);
+        return interned_string;
+    }
+
     return allocate_string(chars, length,hash);
 }
 
--- lib/table.c
+++ lib/table.c
@@ -27,7 +27,14 @@ bool table_set(Table* table, ObjString* key, Value value){
 
     Entry* entry = find_entry(table->entries, table->capacity,key);
     bool is_new_key = entry->key == NULL;
-    if(is_new_key) table->count++;
+
+    /* 
+        count = NUMBER_OF_ENTRIES + TOMBSTONES
+        which is why we only increase the count of the table
+        if the entry was not a tombstone, this is how we keep the 
+        load factor in control
+    */
+    if(is_new_key && IS_NIL(entry->value)) table->count++;
 
     entry->key = key;
     entry->value = value;
@@ -38,9 +45,23 @@ static Entry* find_entry(Entry* entries, int capacity,
                                          ObjString* key){
     /*this is how a bucket is found, the modulus*/
     uint32_t index = key->hash % capacity;
+    Entry* tombstone = NULL;
     for(;;){
         Entry* entry = &entries[index];
         if(entry->key == key || entry->key == NULL){
+            return entry;
+        }
+
+        if(entry->key == NULL){
+            if(IS_NIL(entry->value)){
+                /*
+                    we want to make use of the tombstone incase there's one
+                */
+                return tombstone != NULL ? tombstone : entry; 
+            }else {
+                tombstone = entry;
+            }
+        }else if(entry->key == key){
             return entry;
         }
 
@@ -55,7 +76,96 @@ static void adjust_capacity(Table* table, int capacity){
         entries[i].value = NIL_VAL;
     }
 
+    /*
+        we reset the table count because we probably have
+        tombstones, while rebuilding the table we do not need them
+        so the count is probably inflated
+    */
+    table->count = 0;
+    for (int i = 0; i < table->capacity; i++){
+        Entry* entry = &table->entries[i];
+        if(entry->key == NULL) continue;
+
+        /*
+            here is abit confusing but what happens is;
+
+            by passing the new array (entries) instead of table->entries
+
+            we get access to a pointer into entries thru find_entry() [dest]
+            then we set the old entry (entry) to the new bucket (dest)
+
+            hence writing to the new array (entries)
+
+        */
+
+        /*
+            here we are looking for the bucket where a certain key is supposed to 
+            go in the new array
+
+            hence the rebuilding 
+        */
+        Entry* dest = find_entry(entries,capacity,entry->key);
+        dest->key = entry->key;
+        dest->value = entry->value;
+
+        /*only increment the count if we actually add an entry*/
+        table->count++;
+    }
+    
+    FREE_ARRAY(Entry, table->entries,table->capacity);
     table->entries = entries;
     table->capacity = capacity;
-    
+}
+
+void table_add_all(Table* from, Table* to){
+    for (int i = 0; i < from->capacity; i++){
+        Entry* entry = &from->entries[i];
+        if(entry->key != NULL){
+            table_set(to, entry->key, entry->value);
+        }
+    }
+}
+
+bool table_get(Table* table, ObjString* key, Value* value){
+    if(table->count == 0) return false;
+
+    Entry* entry = find_entry(table->entries,table->capacity,key);
+
+    if(entry->key == NULL) return false;
+
+    *value = entry->value;
+    return true;
+}
+
+bool table_delete(Table* table, ObjString* key){
+    if(table->count == 0) return false;
+
+    Entry* entry = find_entry(table->entries, table->capacity, key);
+    if(entry->key == NULL) return false;
+
+    entry->key = NULL;
+    entry->value = BOOL_VAL(true);
+    return true;
+}
+
+ObjString* table_find_string(Table* table, const char* chars, int length, uint32_t hash){
+    if(table->count == 0) return NULL;
+
+    uint32_t index = hash % table->capacity;
+    for(;;){
+        Entry* entry = &table->entries[index];
+
+        if(entry->key == NULL){
+            /*
+                the entry is genuinely NULL
+            */
+            if(IS_NIL(entry->value)) return NULL;
+        }else if (entry->key->length == length 
+                    && entry->key->hash == hash
+                    && memcmp(entry->key->chars, chars, length) == 0){
+            return entry->key;
+        }
+
+        index = (index + 1) % table->capacity;
+    }
 }
